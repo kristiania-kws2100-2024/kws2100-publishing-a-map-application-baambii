b94068930c6f8694dd8fc9d513fbd9e3b879044a
baambii <113696826+baambii@users.noreply.github.com>
2024-04-04 02:18:42 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom'; 
-import App from './App';
+import App from './App.tsx';
 
 createRoot(document.getElementById('app')).render(<App />);
