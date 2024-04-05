0474a73d6abb6af015c0128e79f42e718edfdb9e
baambii <113696826+baambii@users.noreply.github.com>
2024-04-05 22:32:39 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/kws2100-publishing-a-map-application-baambii/',
   plugins: [react()],
-});
+})
