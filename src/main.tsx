b9f86af358199da9fe0a4b3b860668945e4cc4ce
baambii <nora.sb08@gmail.com>
2024-04-05 21:20:04 +0200

    Include Husky configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom'; 
+import ReactDOM from 'react-dom';
 import App from './App';
 
-createRoot(document.getElementById('app')).render(<App />);
+ReactDOM.render(<App />, document.getElementById('app'));
