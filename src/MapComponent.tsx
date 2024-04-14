717ff5cd1d1fed43eab6ac810b5493b0ffcc870f
baambii <113696826+baambii@users.noreply.github.com>
2024-04-14 13:24:20 +0200

    Update MapComponent.tsx

--- src/MapComponent.tsx
+++ src/MapComponent.tsx
@@ -54,7 +54,7 @@ const MapComponent: React.FC<Props> = () => {
 
     const emergencySheltersLayer = new VectorLayer({
       source: new VectorSource({
-        url: 'https://kristiania-kws2100-2024.github.io/kws2100-publishing-a-map-application-baambii/public/Offentligetilfluktsrom2.geojson', 
+        url: 'https://raw.githubusercontent.com/kristiania-kws2100-2024/kws2100-publishing-a-map-application-baambii/18eceff7a8a1aab3fb2ef850e27dbbccfe24986b/public/Offentligetilfluktsrom2.geojson', // Endre URL-en til å peke på GitHub-raw-URLen
         format: new GeoJSON(),
       }),
       style: function (feature) {
@@ -81,11 +81,9 @@ const MapComponent: React.FC<Props> = () => {
     map.addLayer(civilDefenceLayer);
     map.addLayer(emergencySheltersLayer);
 
-   
     map.on('click', function (event) {
       const coordinate = event.coordinate;
       console.log('Clicked coordinate:', coordinate);
-    
     });
 
     return () => {
